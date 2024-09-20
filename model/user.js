c2aa6b6b8434d1cd0390b0ae451db092e5af5407
Purwatsate <ant.pwt@gmail.com>
2024-09-21 00:48:18 +0630

    update config

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database');
+const sequelize = require('../helper/database');
 
 const User = sequelize.define('User', {
   firstName: {
